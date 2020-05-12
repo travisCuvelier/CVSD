e1cd1b47bb80b9be0a95ff7faa4e494a1a966ece
travisCuvelier <tcuvelier@utexas.edu>
2020-05-12 16:50:37 -0500

    removed demo filename

--- demo.m
+++
@@ -1,7 +0,0 @@
-%Demo of the CVSD scripts
-[pcm,fsOrig] = audioread('wasteLand.wav');
-symbols= cvsdEncode(pcm,fsOrig);
-reconstruction = cvsdDecode(symbols);
-fReconstruction = 16000;
-reconstructionResampled = resample(reconstruction,fsOrig,fReconstruction);
-sound(reconstructionResampled,fsOrig)
